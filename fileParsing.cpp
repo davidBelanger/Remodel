c13002c61e13a98c192487bbb383a35c30709225
davidBelanger <david.b.belanger@gmail.com>
2013-11-21 21:51:10 -0500

    tested the parsing

--- fileParsing.cpp
+++ fileParsing.cpp
@@ -6,14 +6,17 @@
 #include <boost/tokenizer.hpp>
 
 
-
 using namespace boost;
 using namespace std;
-
 /*
-const struct GrammarRegexes{
-  const boost::regex re1("ello");
-};
+
+string joinStringVector(const vector<string> vec){
+  //  char* delimm = ",";
+  //    stringstream res;
+  //  copy(vec.begin(), vec.end(), ostream_iterator<string>(res, ","));
+  //  return res.str();
+  return boost::algorithm::join(vec, ",");
+}
 */
 
 vector<string> parseCommaDeliminatedString(string text){
@@ -32,6 +35,11 @@ vector<string> parseCommaDeliminatedString(string text){
   return fields;
 }
 
+void printVector(vector<string> v){
+  for(int i = 0; i < v.size(); i++){
+    printf("%s ",v[i].c_str());
+  }
+}
 vector<CompilationDependency> processRemodelFile(string filename){
   vector<CompilationDependency> v;
   
@@ -39,35 +47,47 @@ vector<CompilationDependency> processRemodelFile(string filename){
   string content;
   
 
-  boost::regex re("(.*) <- (.+):\\s*\"(.*)\"");
-
+  boost::regex getProductionRegex("(.*) <- (.+):\\s*\"(.*)\"");
+  boost::regex getDefaultLineRegex("DEFAULT\\s*<-\\s*(.+)");
+  
 
   while(getline(file,content)) {
     boost::match_results<std::string::const_iterator> results;
-    
-    if(boost::regex_search(content, results, re)){
+    if(boost::regex_search(content, results, getDefaultLineRegex)){
+      CompilationDependency dep;
+      dep.filename = "DEFAULT";
+      vector<string> res;
+      res.push_back(results[1]);
+      dep.dependencies = res;
+      dep.compilationCommand = "root";
+      v.push_back(dep);
+      
+    }else if(boost::regex_search(content, results, getProductionRegex)){
+      //      printf("match is %s ---- %s ---- %s\n",results[1].c_str(),results[2].c_str(),results[3].c_str());
       string command = results[3];
-
       vector<string> targets =  parseCommaDeliminatedString(results[1]);
       vector<string> dependencies =  parseCommaDeliminatedString(results[2]);
       
       for(int i = 0; i < targets.size(); i++){
 	CompilationDependency dep;
 	dep.filename = targets[i];
+	cout << "dependencies = ";
+	printVector(dependencies);
+	cout << endl;
 	dep.dependencies = dependencies;
 	dep.compilationCommand = command;
 	v.push_back(dep);
       }
-
-      for(int i = 0; i < v.size(); i++){
-	CompilationDependency depp = v[i];
-	printf("got target %s and dependencies %s, %s and command %s\n",depp.filename.c_str(),depp.dependencies[0].c_str(),depp.dependencies[1].c_str(),depp.compilationCommand.c_str());
-
-      }
-
-
     }
+
   }
+  for(int i = 0; i < v.size(); i++){
+    CompilationDependency depp = v[i];
+    printf("got target %s and command %s\n",depp.filename.c_str(),depp.compilationCommand.c_str());
+    printf("with dependencies "); printVector(depp.dependencies); cout << endl;
+  }
+
+
     
   return v;
 }
