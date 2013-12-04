68cbd04d7eeeb3bd1c3d3149374f1d8a5ed5677f
David Belanger <david.b.belanger@gmail.com>
2013-12-04 10:52:32 -0500

    progress

--- test.cpp
+++ test.cpp
@@ -21,47 +21,45 @@ using namespace std;
 
 void buildInParallel(map<string,bool> fs, StringToDepNodeMap dnMap){
   //for each node in the dependency graph, make a function node
-
+  //todo: add logic where it only finds the relevant parts of the tree to traverse (this can just be a flat list of names)
+  //todo: add logic where if everything is fresh, then you don't build at all.
   graph g;
   broadcast_node<bool> input(g);
-  map<string,function_node<bool,bool> > functionNodes; 
-  for (map<string,DependencyNode*>::iterator it = dnMap.begin();
-        it != dnMap.end();
-       ++it){
+  map<string,function_node<bool,bool>* > functionNodes; 
+  for (map<string,DependencyNode*>::iterator it = dnMap.begin(); it != dnMap.end(); it++){
     string name = it -> first;
     bool fileHasNotChangedOnDisk = fs[name];
     DependencyNode* node = dnMap[name];
     node -> fileHasChanged = !fileHasNotChangedOnDisk;
     
-    function_node<bool,bool> f( g, unlimited, [=](bool a ) -> bool{ return node->build(a);});
-    map[node->target]= f;
+    function_node<bool,bool> * f = new function_node<bool,bool>( g, unlimited, [=](bool a ) -> bool{ return node->build(a);});
+    functionNodes[node->target]= f;
   }
 
-  map<string,function_node<bool,bool> >::iterator iter;
+  map<string,function_node<bool,bool>* >::iterator iter;
   for(iter = functionNodes.begin(); iter != functionNodes.end(); iter++){
-    function_node<bool,bool> node = iter->second;
-    vector<DependencyNode*>::
-    make_edge(parent,node)
+    DependencyNode* depNode = dnMap[iter->first];
+    if(depNode->target != iter->first){
+      printf("problem\n");
+    }
+    function_node<bool,bool>*node  = iter->second;
+    //if it is a leaf node, connect it to the input node, else connect it to its parents
+    printf("making an edge between input and %s @ %p and %p\n",(*depNode).target.c_str(), &input, node);
+    // make_edge(input,*node);
+    if(depNode->dependencies.size() == 0)
+      make_edge(input,*node);
+    for(int i = 0; i < depNode->dependencies.size(); i++){
+      function_node<bool,bool>* parent = functionNodes[ depNode->dependencies[i]->target];
+      make_edge(*parent,*node);
+    } 
+    
+
   }
 
+  input.try_put(false);
 
-   
-    make_edge( input, squarer );
-  make_edge( input, cuber );
-  make_edge( squarer, get<0>( join.input_ports() ) );
-  make_edge( cuber, get<1>( join.input_ports() ) );
-  make_edge( join, summer );
-
-  for (int i = 1; i <= 10; ++i)
-    input.try_put(i);
   g.wait_for_all();
-
 	
-  //now, loop over the nodes again and make an edge between it and its parents (dependencies)
-
-  
-  //now, run
-
 }
 
 int main() {
